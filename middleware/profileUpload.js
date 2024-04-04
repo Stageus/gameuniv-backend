054cea92e810fbb322eb9c1b002b65af1cd9cf1f
jochongs <joch2712@naver.com>
2024-04-04 17:13:14 +0900

    refactor: Update profileUpload middleware

--- middleware/profileUpload.js
+++ middleware/profileUpload.js
@@ -9,7 +9,7 @@ AWS.config.update(awsConfig);
 const uploadMulter = multer({
   storage: multerS3({
     s3: new AWS.S3(),
-    bucket: 'jochong/gameuniv_user_profile',
+    bucket: `${process.env.S3_BUCKET}/gameuniv_user_profile`,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
       const randomNumber = makeRandomNumber(6);
